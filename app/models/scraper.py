315de8919b136d5bb0c8215a8861f15bf8ca1d69
lucas1224420126 <38039990+lucas1224420126@users.noreply.github.com>
2024-07-07 13:08:31 -0300

    Adicionando mais possibilidades para extrair os dados

--- app/models/scraper.py
+++ app/models/scraper.py
@@ -1,36 +1,50 @@
 import requests
-from datetime import datetime
-from bs4 import BeautifulSoup
-import pandas as pd
 
-
-def read_csv_file(filename: str):
-    if not file_path.exists():
-        raise HTTPException(status_code=404, detail="File not found")
-    
-    df = pd.read_csv(file_path)
-    return df.to_csv(index=False)
-
-
-def producao_vinicola():
-    """Função para fazer o download do arquivo CSV com os dados de produção vinícola."""	
-    url = 'http://vitibrasil.cnpuv.embrapa.br/download/Producao.csv'
+def download_csv(url):
+    """Função para fazer o download de um arquivo CSV."""
     response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
     # Verificar se o download foi bem-sucedido
-    if response.status_code == 200:
-        # Salvar o conteúdo em um arquivo local
-        name_file = fr'..\files\producao_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv'
-        with open(name_file, 'wb') as arquivo:
-            arquivo.write(response.content)
-        final_content = read_csv_file(name_file)
-    else:
-        raise HTTPException(status_code=404, detail="Falha no download do arquivo.")
-    
-    return final_content
+    try:
+        if response.status_code == 200:
+            content = response.content
+            return content
+        else:
+            print("Erro no download do arquivo")
+    except Exception as e:
+        print(f"Erro: {e}")
 
 
 def scrape_viticulture_data(category):
-    
-    response = producao_vinicola()
+    print(category)
+    try:
+        response = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/Producao.csv') if category == "Produção" else None # Realizando o download do CSV de Produção
+        
+        if category == "Processamento": # Realizando o download dos arquivos de Processamento
+            ProcessaViniferas = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ProcessaViniferas.csv') # Realizando o download dos arquivos CSV
+            ProcessaAmericanas = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ProcessaAmericanas.csv') # Realizando o download dos arquivos CSV
+            ProcessaMesa = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ProcessaMesa.csv') # Realizando o download dos arquivos CSV
+            ProcessaSemclass = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ProcessaSemclass.csv') # Realizando o download dos arquivos CSV
+            response = [ProcessaViniferas, ProcessaAmericanas, ProcessaMesa, ProcessaSemclass] #Juntando todos os arquivos em um único retorno
+
+        response = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/Comercio.csv') if category == "Comercialização" else None # Realizando o download do CSV de Comercio
+
+        if category == "Importação": # Realizando o download dos arquivos de Importacao
+            ImpVinhos = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ImpVinhos.csv') # Realizando o download dos arquivos CSV
+            ImpEspumantes = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ImpEspumantes.csv') # Realizando o download dos arquivos CSV
+            ImpFrescas = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ImpFrescas.csv') # Realizando o download dos arquivos CSV
+            ImpPassas = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ImpPassas.csv') # Realizando o download dos arquivos CSV
+            ImpSuco = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ImpSuco.csv') # Realizando o download dos arquivos CSV
+            response = [ImpVinhos, ImpEspumantes, ImpFrescas, ImpPassas, ImpSuco] #Juntando todos os arquivos em um único retorno
+
+        if category == "Exportação": # Realizando o download dos arquivos de Exportacao
+            ExpVinho = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ExpVinho.csv')# Realizando o download dos arquivos CSV
+            ExpEspumantes = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ExpEspumantes.csv')# Realizando o download dos arquivos CSV
+            ExpUva = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ExpUva.csv')# Realizando o download dos arquivos CSV
+            ExpSuco = download_csv('http://vitibrasil.cnpuv.embrapa.br/download/ExpSuco.csv') # Realizando o download dos arquivos CSV
+            response = [ExpVinho, ExpEspumantes, ExpUva, ExpSuco] #Juntando todos os arquivos em um único retorno
+        #Processamento Comercialização Importação Exportação
+    except Exception as e:
+        print(f"Erro: {e}")
     
     return response
